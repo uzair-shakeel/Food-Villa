86fe1f2928e080ecbf3687046230f9319865a223
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-02-07 00:30:25 +0500

    index logo added

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { useCartContext } from "../context/cartContext";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cartItem, addToCart, removeFromCart } = useCartContext();
+  return (
+    <div className="pt-14">
+      <div className={cartItem?.length === 0 ? "bg-orange h-96" : "bg-orange"}>
+        <div className="container mx-auto px-5 py-6">
+          <div className="w-full bg-white px-10 py-5  text-black rounded-md">
+            <div className="flex justify-between border-b pb-8">
+              <h1 className="font-semibold text-2xl">My Food Cart</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
