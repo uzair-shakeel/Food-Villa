546dce0db31650e33d4975066b6c705fe1a43369
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-02-15 23:29:10 +0500

    cors error

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,16 @@ mongoose
   .catch((err) => console.log(err));
 
 // Middleware for CORS and JSON parsing
+const allowedOrigins = ["https://food-villa-five.vercel.app"];
 app.use(
   cors({
-    origin: "https://food-villa-five.vercel.app",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
 
