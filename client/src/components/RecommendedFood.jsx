ee74b99cd5d0a1b545022b775be5da19c87c1e7f
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-02-15 07:56:42 +0500

    .

--- client/src/components/RecommendedFood.jsx
+++ client/src/components/RecommendedFood.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import useFetch from "../hooks/useFetch";
 import FoodCard from "../components/FoodCard";
+import BASE_URL from "../utils/config.js";
 
 const RecommendedFood = () => {
-  const BASE_URL = process.env.REACT_APP_BASE_URL;
   const { apiData: foods, error } = useFetch(`${BASE_URL}/food/elite`);
-
+  console.log(BASE_URL);
   return (
     <section className="py-3 px-10 sm:px-4 md:px-6">
       <div className="container mx-auto py-[2vh]">
