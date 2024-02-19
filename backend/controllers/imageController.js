7ccd29a5fa7a94fd0ee30a21723ff2a7f3e842bb
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-02-19 10:05:18 +0500

    ..

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,19 +1,38 @@
-const cloudinary = require("cloudinary");
+const serviceAccount = require("./../serviceAccountKey.json");
 
-cloudinary.config({
-  cloud_name: "mern-practice",
-  api_key: "748289359289231",
-  api_secret: "Qz_0OA9kSwfu0sV5DVCYet2TfHc",
+// Initialize Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "gs://food-villa-89cbc.appspot.com",
 });
 
 const imageController = async (req, res) => {
   try {
     if (req.files && req.files.image) {
-      const result = await cloudinary.uploader.upload(req.files.image.path);
-      res.json({
-        url: result.secure_url,
-        public_id: result.public_id,
+      const file = req.files.image;
+      const bucket = admin.storage().bucket();
+      const metadata = {
+        metadata: {
+          contentType: file.mimetype,
+        },
+      };
+
+      const uploadTask = bucket.upload(file.path, {
+        metadata: metadata,
       });
+
+      uploadTask
+        .then((data) => {
+          const fileUrl = `https://storage.googleapis.com/${bucket.name}/${data[0].name}`;
+          res.json({
+            url: fileUrl,
+            public_id: data[0].metadata.id,
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).json({ error: "Failed to upload image to Firebase" });
+        });
     } else {
       res.status(400).json({ error: "No image file provided" });
     }
