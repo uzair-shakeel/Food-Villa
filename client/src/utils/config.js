89c71b7f2609d1c245be82dfd8b3e89802f1d1e7
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-02-20 15:46:49 +0500

    .

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://food-villa-backend.vercel.app";
-// const BASE_URL = "http://localhost:3000";
+// const BASE_URL = "https://food-villa-backend.vercel.app";
+const BASE_URL = "http://localhost:3000";
 //
 export default BASE_URL;
